985f686c58e446bb38de4af8b6138db920d6a5e5
Christopher Leung <minecraft200times@gmail.com>
2023-04-04 11:40:14 -0400

    Update main.py
    changed contains to find

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def PrintHelp(message):
 
 # Returns the ID of the item
 def GetItem(name):
-  if(name.contains("’")):
+  if(name.find("’")):
     name.replace("’","'")
   name = name.strip().lower()
   url = "https://prices.runescape.wiki/api/v1/osrs/mapping"
@@ -57,7 +57,7 @@ def GetItem(name):
 
 # Returns the low and high price of the item
 def GetPrice(name):
-  if(name.contains("’")):
+  if(name.find("’")):
     name.replace("’","'")
   id = GetItem(name)
   if id == -1:
@@ -77,7 +77,7 @@ def GetPrice(name):
 # Adds or Removes from the daily list
 async def ChangeItem(message, name, type):
   await message.channel.send("Processing")
-  if(name.contains("’")):
+  if(name.find("’")):
     name.replace("’","'")
   name = name.strip().lower()
   collection_name = mongoclient["profiles"][str(message.author.id)]
